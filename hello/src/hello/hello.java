10a9be0287a9aae97433bc1df67251e3096a8798
masuamasumasuk <masujjing@gmail.com>
2023-09-09 21:16:16 +0900

    ddd

---
+++ hello/src/hello/hello.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, World! ");
+		System.out.print("Hello, World! ");
+		System.out.println(" Difference between println and print");
+	}
+
+}
