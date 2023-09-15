25188ec25031238758242c499624848d0123ef08
MediaLab <MediaLab@DESKTOP-DPOLIG3>
2023-09-15 10:35:13 +0900

    CastingTest1

--- hello/src/hello/CatingTest1.java
+++ hello/src/hello/CatingTest1.java
@@ -9,7 +9,7 @@ public class CatingTest1 {
 		System.out.println("확대 형 변환 : "+i);
 		int j = 259;
 		double d = 259.428;
-		System.out.println("축소 형 변환 : "+j);
+		System.out.println("\n 축소 형 변환 결과");
 		b = (byte) j;
 		System.out.println("int 259를 byte로 : " + b);
 		i = (int) d;
