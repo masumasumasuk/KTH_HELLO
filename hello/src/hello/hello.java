a97545c4db751e3bfc3591e9530878316e68fd94
MediaLab <MediaLab@DESKTOP-DPOLIG3>
2023-09-15 10:24:25 +0900

    hello

--- hello/src/hello/hello.java
+++ hello/src/hello/hello.java
@@ -1,10 +1,20 @@
 package hello;
 
 public class hello {
+	public static int sum(int n, int m) {
+		return n + m;
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Hello, World! ");
-		System.out.print("Hello, World! ");
-		System.out.println(" Difference between println and print");
+		int i = 20;
+		int s;
+		char a;
+		
+		s = sum(i,10);
+		a = '?';
+		System.out.println(a);
+		System.out.println("Hello");
+		System.out.println(s);
 	}
 
 }
