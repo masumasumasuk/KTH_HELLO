11941470a1118f5873e6e14cd3bbf76ee792ba81
MediaLab <MediaLab@DESKTOP-DPOLIG3>
2023-09-15 11:40:40 +0900

    2주차

---
+++ hello/src/hello/shiftOPTest1.java
@@ -0,0 +1,18 @@
+package hello;
+
+import java.util.Scanner;
+
+public class shiftOPTest1 {
+	public static void main(String args[])
+	{
+		Scanner stdin = new Scanner(System.in);
+		System.out.print("한 개의 숫자를 입력 : ");
+		int a = stdin.nextInt();
+		
+		boolean flag;
+		flag = (a % 2 == 0) ? true : false;
+		System.out.print(a +"이(가) 짝수입니까? : ");
+		System.out.print(flag);
+	}
+
+}
